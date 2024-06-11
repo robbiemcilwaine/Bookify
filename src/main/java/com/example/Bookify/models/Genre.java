e7cf73115cc13c32e71bb1be300de8a3c6e49778
KhanZuh <khanzuhair96@live.co.uk>
2024-06-11 11:27:00 +0100

    enums generated

--- src/main/java/com/example/Bookify/models/Genre.java
+++ src/main/java/com/example/Bookify/models/Genre.java
@@ -1,4 +1,21 @@
 package com.example.Bookify.models;
 
 public enum Genre {
+    FICTION,
+    NONFICTION,
+    MYSTERY,
+    FANTASY,
+    BIOGRAPHY,
+    SCIENCE,
+    HISTORY,
+    ROMANCE,
+    HORROR,
+    SELF_HELP
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/Bookify/models/ReadingStatus.java
+++ src/main/java/com/example/Bookify/models/ReadingStatus.java
@@ -1,4 +1,14 @@
 package com.example.Bookify.models;
 
 public enum ReadingStatus {
+    TO_READ,
+    CURRENTLY_READING,
+    READ
+
+
+
+
+
+
+
 }
