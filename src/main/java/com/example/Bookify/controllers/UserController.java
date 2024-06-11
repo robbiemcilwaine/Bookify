badceaba6cb6b2d5e92d29a6938e7321919a1814
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-11 16:45:27 +0100

    added user controller

--- src/main/java/com/example/Bookify/controllers/UserController.java
+++ src/main/java/com/example/Bookify/controllers/UserController.java
@@ -1,4 +1,50 @@
 package com.example.Bookify.controllers;
 
+
+import com.example.Bookify.models.User;
+import com.example.Bookify.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+//    Retrieve users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+
+//    Get users by id
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>>getUserById(@PathVariable long id){
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+//Update the details of a specific user
+
+    
+
+
+
 }
