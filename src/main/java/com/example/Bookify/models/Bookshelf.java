707f80aea3deda13e58b7713464e5e6b1ec5afc0
Charisma <actuallycharisma@outlook.com>
2024-06-11 14:13:25 +0100

    finished all models

--- src/main/java/com/example/Bookify/models/Bookshelf.java
+++ src/main/java/com/example/Bookify/models/Bookshelf.java
@@ -3,6 +3,7 @@ package com.example.Bookify.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -17,15 +18,64 @@ public class Bookshelf {
     @Column
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @ManyToMany
-    @JoinTable(name = "bookshelves_books",
-            @JoinColumn(name = "bookshelf_id"),
-            @JoinColumn(name = "book_id")
-    )
+    @JoinTable(
+            name = "bookshelf_books",
+            joinColumns = @JoinColumn(name = "bookshelf_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")    )
+    private List<Book> books;
+
 //    @OneToMany(mappedBy = "bookshelves")
 //    @JoinColumn(name = "bookshelves_id")
 //
 //    @JsonIgnoreProperties("{bookshelves}")
 //    private List<Book> books;
+
+
+    public Bookshelf(long id, User user, String name) {
+        this.id = id;
+        this.user = user;
+        this.name = name;
+        this.books = new ArrayList<>();
+    }
+
+    public Bookshelf() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 
 }
--- src/main/java/com/example/Bookify/models/UsersBooks.java
+++ src/main/java/com/example/Bookify/models/UsersBooks.java
@@ -1,4 +1,66 @@
 package com.example.Bookify.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users_books")
 public class UsersBooks {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Enumerated(EnumType.STRING)
+    private ReadingStatus readingStatus;
+
+    public UsersBooks(long id, User user, Book book, ReadingStatus readingStatus) {
+        this.id = id;
+        this.user = user;
+        this.book = book;
+        this.readingStatus = readingStatus;
+    }
+
+    public UsersBooks() {
+
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ReadingStatus getReadingStatus() {
+        return readingStatus;
+    }
+
+    public void setReadingStatus(ReadingStatus readingStatus) {
+        this.readingStatus = readingStatus;
+    }
 }
