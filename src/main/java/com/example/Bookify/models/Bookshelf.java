e7b0a6078ccd2c9dfc7a257ec66d062f00c758e4
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-11 13:38:05 +0100

    made half of bookshelf model

--- src/main/java/com/example/Bookify/models/Bookshelf.java
+++ src/main/java/com/example/Bookify/models/Bookshelf.java
@@ -1,4 +1,31 @@
 package com.example.Bookify.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "bookshelves")
 public class Bookshelf {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "bookshelves_books",
+            @JoinColumn(name = "bookshelf_id"),
+            @JoinColumn(name = "book_id")
+    )
+//    @OneToMany(mappedBy = "bookshelves")
+//    @JoinColumn(name = "bookshelves_id")
+//
+//    @JsonIgnoreProperties("{bookshelves}")
+//    private List<Book> books;
+
 }
