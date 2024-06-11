e76857d2240bcde224a71902a528d8f6f58b124e
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-11 17:07:08 +0100

    added details of specific user in controller

--- src/main/java/com/example/Bookify/controllers/UserController.java
+++ src/main/java/com/example/Bookify/controllers/UserController.java
@@ -2,14 +2,12 @@ package com.example.Bookify.controllers;
 
 
 import com.example.Bookify.models.User;
+import com.example.Bookify.models.UserDTO;
 import com.example.Bookify.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,8 +41,19 @@ public class UserController {
 
 //Update the details of a specific user
 
-    
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> updateUser (@PathVariable long id, @RequestBody UserDTO userDTO){
+        Optional<User> updatedUser = userService.updateUser(id, userDTO);
+        return new ResponseEntity<>(updatedUser,HttpStatus.OK);
+    }
 
+
+//Create a new user
+
+
+
+
+// Delete a specific user
 
 
 }
