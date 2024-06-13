f224db4caacba068b70d9bc61381dcad87fb1fef
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-13 17:08:48 +0100

    clean data loader

--- src/main/java/com/example/Bookify/components/DataLoader.java
+++ src/main/java/com/example/Bookify/components/DataLoader.java
@@ -37,70 +37,66 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+//        Create users and add them to the database through service method createUser
         User robbie = new User("Zinizanzo");
         userService.createUser(robbie);
-
-
-        Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "978-0-06-112008-4", LocalDate.of(1960, 7, 11), 4.28, Genre.FICTION);
-        Book book2 = new Book("1984", "George Orwell", "978-0-452-28423-4", LocalDate.of(1949, 6, 8), 4.17, Genre.SCIENCE_FICTION);
-        Book book3 = new Book("Moby Dick", "Herman Melville", "978-0-14-243724-7", LocalDate.of(1851, 11, 14), 3.5, Genre.FICTION);
-        Book book4 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0-7432-7356-5", LocalDate.of(1925, 4, 10), 3.92, Genre.FICTION);
-        Book book5 = new Book("Pride and Prejudice", "Jane Austen", "978-0-19-280238-5", LocalDate.of(1813, 1, 28), 4.26, Genre.ROMANCE);
-
-        bookRepository.save(book1);
-        bookRepository.save(book2);
-        bookRepository.save(book3);
-        bookRepository.save(book4);
-        bookRepository.save(book5);
-
-        List<Book> robbieBooks = new ArrayList<>();
-
-        Bookshelf bookshelf1 = new Bookshelf(robbie,"Robbie's Bookshelf");
-
-
-        bookshelfService.createBookshelf(bookshelf1);
-        bookshelfService.addBookToBookshelf(1,book1);
-        bookshelfService.addBookToBookshelf(1,book2);
-        bookshelfService.addBookToBookshelf(1,book3);
-        bookshelfService.addBookToBookshelf(1,book4);
-        bookshelfService.addBookToBookshelf(1,book5);
-
-        UsersBooks newUsersBook2 = new UsersBooks(robbie, book1);
-        usersBooksRepository.save(newUsersBook2);
-        UsersBooks newUsersBook3 = new UsersBooks(robbie, book2);
-        usersBooksRepository.save(newUsersBook3);
-        UsersBooks newUsersBook4 = new UsersBooks(robbie, book3);
-        usersBooksRepository.save(newUsersBook4);
-
-
         User charisma = new User("CHRIMPY");
         userService.createUser(charisma);
-        Book book6 = new Book("A Day of Fallen Night", "Samantha Shannon", "978-1-5266-1981-5", LocalDate.of(2023, 2, 28), 4.4, Genre.FANTASY);
-        Bookshelf bookshelf2 = new Bookshelf(charisma, "Charisma's bookshelf");
-        bookshelfService.createBookshelf(bookshelf2);
+        User sabah = new User("Flakeymeerkat");
+        userService.createUser(sabah);
 
+//        Create new bookshelf for first two users, .... saving them to the database through service method createBookshelf
+        Bookshelf robbiesBookshelf = new Bookshelf(robbie,"Robbie's Bookshelf");
+        bookshelfService.createBookshelf(robbiesBookshelf);
+        Bookshelf charismasBookshelf = new Bookshelf(charisma, "Charisma's bookshelf");
+        bookshelfService.createBookshelf(charismasBookshelf);
+
+//        Create books, save them to the database and add them to bookshelves
+//        Robbie's books
+        Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", "978-0-06-112008-4", LocalDate.of(1960, 7, 11), 4.28, Genre.FICTION);
+        bookRepository.save(book1);
+        Book book2 = new Book("1984", "George Orwell", "978-0-452-28423-4", LocalDate.of(1949, 6, 8), 4.17, Genre.SCIENCE_FICTION);
+        bookshelfService.addBookToBookshelf(1,book1);
+        bookRepository.save(book2);
+        bookshelfService.addBookToBookshelf(1,book2);
+        Book book3 = new Book("Moby Dick", "Herman Melville", "978-0-14-243724-7", LocalDate.of(1851, 11, 14), 3.5, Genre.FICTION);
+        bookRepository.save(book3);
+        bookshelfService.addBookToBookshelf(1,book3);
+        Book book4 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0-7432-7356-5", LocalDate.of(1925, 4, 10), 3.92, Genre.FICTION);
+        bookRepository.save(book4);
+        bookshelfService.addBookToBookshelf(1,book4);
+        Book book5 = new Book("Pride and Prejudice", "Jane Austen", "978-0-19-280238-5", LocalDate.of(1813, 1, 28), 4.26, Genre.ROMANCE);
+        bookRepository.save(book5);
+        bookshelfService.addBookToBookshelf(1,book5);
+
+//        Charisma's book
+        Book book6 = new Book("A Day of Fallen Night", "Samantha Shannon", "978-1-5266-1981-5", LocalDate.of(2023, 2, 28), 4.4, Genre.FANTASY);
         bookRepository.save(book6);
         bookshelfService.addBookToBookshelf(2, book6);
 
-        UsersBooks newUsersBook1 = new UsersBooks(charisma, book6);
-        usersBooksRepository.save(newUsersBook1);
-
-
-        User sabah = new User("Flakeymeerkat");
-        userService.createUser(sabah);
         Book book7 = new Book("The Hobbit", "J.R.R. Tolkien", "978-0-618-00221-3", LocalDate.of(1937, 9, 21), 4.27, Genre.FANTASY);
-        Book book8 = new Book("Crime and Punishment", "Fyodor Dostoevsky", "978-0-14-305814-4", LocalDate.of(1866, 1, 1), 4.21, Genre.FICTION);
-
         bookRepository.save(book7);
+
+//        Sabah's book
+        Book book8 = new Book("Crime and Punishment", "Fyodor Dostoevsky", "978-0-14-305814-4", LocalDate.of(1866, 1, 1), 4.21, Genre.FICTION);
         bookRepository.save(book8);
 
-//        ReadingStatus readingStatusSabah = ReadingStatus.CURRENTLY_READING;
+//        Create UserBooks objects and assign them a User and a Book
+//        Robbie's UserBooks
+        UsersBooks newUsersBook1 = new UsersBooks(robbie, book1);
+        usersBooksRepository.save(newUsersBook1);
+        UsersBooks newUsersBook2 = new UsersBooks(robbie, book2);
+        usersBooksRepository.save(newUsersBook2);
+        UsersBooks newUsersBook3 = new UsersBooks(robbie, book3);
+        usersBooksRepository.save(newUsersBook3);
 
+//        Charisma's UserBooks
+        UsersBooks newUsersBook4 = new UsersBooks(charisma, book6);
+        usersBooksRepository.save(newUsersBook4);
+
+//        Sabah's UsersBooks
         UsersBooks newUsersBook = new UsersBooks(sabah, book8);
         usersBooksRepository.save(newUsersBook);
-
-
-
 
     }
 
